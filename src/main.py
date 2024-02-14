d64ac886cbc1e4b1f2a4075dc7f25dde38fd7b77
yulguim <y.ulguim@gmail.com>
2024-02-14 17:53:28 -0300

    feat: importação de mini-índice

--- src/main.py
+++ src/main.py
@@ -14,16 +14,14 @@ client = MongoClient("mongodb://192.168.31.188:27017/")
 tickers_list = open('resources/tickers.txt', 'r')
 tickers_list = list(map(lambda x: x.strip(), tickers_list.readlines()))
 
-# tickers_list = ['FRAS3', 'GGPS3']
-# count = 0
-# for line in tickers_list:
-#     count += 1
-#     print("Line{}: {}".format(count, line.strip()))
-
 start = datetime(2022, 1, 1)
 end = datetime.now()
 
 run_daily(tickers_list, start, end)
 run_hourly(tickers_list)
 
+tickers_list = open('resources/win.txt', 'r')
+tickers_list = list(map(lambda x: x.strip(), tickers_list.readlines()))
 
+run_daily(tickers_list, start, end)
+run_hourly(tickers_list)
