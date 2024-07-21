626d6ce07bed56d4a5a0bd80ba1717ec8141bf4c
Yulle Ulguim <yulguim@gmail.com>
2024-07-21 13:14:49 -0300

    feat: global config file

--- src/batch/mt_to_mongo.py
+++ src/batch/mt_to_mongo.py
@@ -1,21 +1,21 @@
-from datetime import datetime
 from pathlib import Path
 
 from pymongo import MongoClient
 
+import config
 from daily import *
 from hourly import *
 
-client = MongoClient("mongodb://192.168.31.188:27017/")
+client = MongoClient(config.MONGODB_URL)
 
-db = client.get_database('b3')
+db = client.get_database(config.MONGODB_DATABASE)
 
 path = Path(__file__).parent / '../resources/tickers.txt'
 
 tickers_list = open(path, 'r')
 tickers_list = list(map(lambda x: x.strip(), tickers_list.readlines()))
 
-start = datetime(2022, 1, 1)
+start = datetime(config.START_YEAR, 1, 1)
 end = datetime.now()
 
 run_daily(tickers_list, start, end, db)
